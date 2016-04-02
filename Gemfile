0118a2c8ef78c087c4848205ba6ec543b84794cc
young <pieonear92@gmail.com>
2016-04-03 04:49:03 +0900

    yes!

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'aws-sdk'
 gem 'aws-sdk-rails'
 
-gem 'carrierwave'
+gem 'carrierwave-aws'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,16 @@
 # Allow non-ascii letters in uploaded filenames
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 
-#CarrierWave.configure do |config|
-#  config.storage    = :aws
-#  config.aws_bucket = ENV['FORLEE_S3_BUCKET_NAME']
-#  config.aws_acl    = :public_read
-#  config.asset_host = 'forlee-db.s3-website-ap-northeast-1.amazonaws.com'
-#  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
-#
-#  config.aws_credentials = {
-#    access_key_id: ENV['FORLEE_AWS_ACCESS_KEY_ID'],
-#    secret_access_key: ENV['FORLEE_AWS_SECRET_KEY']
-#  }
-#end if Rails.env == 'production'
+CarrierWave.configure do |config|
+  config.storage    = :aws
+  config.aws_bucket = 'forlee'
+  config.aws_acl    = :public_read
+	config.asset_host = 'https://s3-ap-northeast-2.amazonaws.com/forlee'
+  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
+
+  config.aws_credentials = {
+    access_key_id: 'AKIAIKHH66Y2OGKMVMCQ',
+    secret_access_key: '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',
+		region: 'ap-northeast-2'
+  }
+end
